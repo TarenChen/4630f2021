ea5d6a5140ed8cdab789806b0d7cf804070a39ff
Taren <taren_chen@student.uml.edu>
2021-11-10 23:18:29 -0500

    bottom nav

--- project/project-repo/app/src/main/java/com/example/myapplication/MainActivity.java
+++ project/project-repo/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.myapplication.fragments.HomeFragment;
+import com.example.myapplication.fragments.ProfileFragment;
+import com.example.myapplication.fragments.RecordFragment;
+import com.example.myapplication.fragments.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_record:
+                            selectedFragment = new RecordFragment();
+                            break;
+                        case R.id.nav_setting:
+                            selectedFragment = new SettingFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
